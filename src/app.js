e87f3ffbb4d4f0a569c0cd08ed09ccccaad45ead
bruferrari <b.ferrari09@gmail.com>
2019-02-04 20:47:40 -0200

    minor changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'react';
+import { createStore } from 'redux';
 import reducers from './reducers';
+import { Container, Left, Right, Body, Header, Title } from 'native-base';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-            <View />
+            <View>
+            <Header>
+                <Left/>
+                <Body>
+                    <Title>Tech Stack</Title>
+                </Body>
+                <Right />
+            </Header>
+                <Container>
+                    <LibraryList />
+                </Container>
+            </View>
         </Provider>
     );
 };
