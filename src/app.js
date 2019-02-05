ac092fe1e3fdf992d2b69045e14afffcf2c22e04
bruferrari <b.ferrari09@gmail.com>
2019-02-05 16:31:49 -0200

    displaying the list of items with redux

--- src/app.js
+++ src/app.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-import { Container, Left, Right, Body, Header, Title } from 'native-base';
-import LibraryList from './components/LibraryList';
+import React from 'react'
+import { View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import { Header } from './components/common'
+import LibraryList from './components/LibraryList'
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
             <View>
-            <Header>
-                <Left/>
-                <Body>
-                    <Title>Tech Stack</Title>
-                </Body>
-                <Right />
-            </Header>
-                <Container>
-                    <LibraryList />
-                </Container>
+                <Header headerText="Tech Stack"/>
+                <LibraryList />
             </View>
         </Provider>
     );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+    renderItem(item) {
+        return <ListItem library={item} />
+    }
+
     render() {
-        console.log(this.props)
-        return;
+        return(
+            <FlatList 
+                data = {this.props.libraries}
+                renderItem={this.renderItem}
+                keyExtractor={library => library.id.toString()}
+            />
+        );
     }
 }
 
 const mapStateToProps = state => {
-    return { libraries: state.libraries };
-};
+    return { libraries: state.libraries }
+}
 
-export default connect(mapStateToProps)(LibraryList);
+export default connect(mapStateToProps)(LibraryList)
