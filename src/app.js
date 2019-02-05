c7cba3eaad906ff7b394e7be3ea6fa92367a248d
bruferrari <b.ferrari09@gmail.com>
2019-02-05 16:34:00 -0200

    make use of the native base lib

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
-import { Header } from './components/common'
-import LibraryList from './components/LibraryList'
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Container, Left, Right, Body, Header, Title } from 'native-base';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
             <View>
-                <Header headerText="Tech Stack"/>
+                <Header>
+                    <Left/>
+                    <Body>
+                        <Title>Tech Stack</Title>
+                    </Body>
+                    <Right />
+                </Header>
+                
                 <LibraryList />
             </View>
         </Provider>
